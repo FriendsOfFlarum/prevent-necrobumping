26382de73768c4c01b9d668772c2060587b302bf
Davide Iadeluca <davide.iadeluca@glowingblue.com>
2025-05-09 12:03:51 +0200

    refactor: improve extensibility

--- js/src/forum/components/NecrobumpingCheck.js
+++ js/src/forum/components/NecrobumpingCheck.js
@@ -3,6 +3,7 @@ import app from 'flarum/forum/app';
 import Component from 'flarum/common/Component';
 import Checkbox from 'flarum/common/components/Checkbox';
 import Stream from 'flarum/common/utils/Stream';
+import ItemList from 'flarum/common/utils/ItemList';
 
 export default class NecrobumpingCheck extends Component {
   oninit(vnode) {
@@ -12,34 +13,55 @@ export default class NecrobumpingCheck extends Component {
   }
 
   view() {
-    const customTitle = app.data['fof-prevent-necrobumping.message.title'];
-    const customDescription = app.data['fof-prevent-necrobumping.message.description'];
-    const customAgreement = app.data['fof-prevent-necrobumping.message.agreement'];
-
-    const time = dayjs().add(this.attrs.days, 'days').fromNow(true);
-
     return (
       <div>
         <div className="Alert">
-          <div className="Alert-body">
-            <div className="hide">
-              <h4>
-                {(customTitle && customTitle.replace(/\[time]/i, time)) ||
-                  app.translator.trans('fof-prevent-necrobumping.forum.composer.warning.title', {
-                    time,
-                  })}
-              </h4>
-
-              <p>{customDescription || app.translator.trans('fof-prevent-necrobumping.forum.composer.warning.description')}</p>
-            </div>
-
-            <Checkbox state={this.checked()} onchange={this.onchange.bind(this)}>
-              {customAgreement || app.translator.trans('fof-prevent-necrobumping.forum.composer.warning.checkbox_label')}
-            </Checkbox>
-          </div>
+          <div className="Alert-body">{this.viewItems().toArray()}</div>
         </div>
       </div>
     );
+  }
+
+  viewItems() {
+    const items = new ItemList();
+
+    const customAgreement = app.data['fof-prevent-necrobumping.message.agreement'];
+
+    items.add('hide', <div className="hide">{this.alertItems().toArray()}</div>, 100);
+
+    items.add(
+      'checkbox',
+      <Checkbox state={this.checked()} onchange={this.onchange.bind(this)}>
+        {customAgreement || app.translator.trans('fof-prevent-necrobumping.forum.composer.warning.checkbox_label')}
+      </Checkbox>,
+      90
+    );
+
+    return items;
+  }
+
+  alertItems() {
+    const items = new ItemList();
+
+    const customTitle = app.data['fof-prevent-necrobumping.message.title'];
+    const customDescription = app.data['fof-prevent-necrobumping.message.description'];
+
+    const time = dayjs().add(this.attrs.days, 'days').fromNow(true);
+
+    items.add(
+      'title',
+      <h4>
+        {(customTitle && customTitle.replace(/\[time]/i, time)) ||
+          app.translator.trans('fof-prevent-necrobumping.forum.composer.warning.title', {
+            time,
+          })}
+      </h4>,
+      100
+    );
+
+    items.add('description', <p>{customDescription || app.translator.trans('fof-prevent-necrobumping.forum.composer.warning.description')}</p>, 90);
+
+    return items;
   }
 
   onchange() {
